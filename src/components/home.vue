8777297bb838658b1797e0dea5389cb2e85c0381
fengjiaxin <2336928126@qq.com>
2021-03-20 22:53:47 +0800

    a

---
+++ src/components/home.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-container  id="container" >
+      <el-header
+        >Header
+        <el-button type="info" @click="logout">退出</el-button></el-header
+      >
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push({ path: "/login" });
+    },
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+#container{
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #373441;
+}
+.el-main {
+  background-color: #82808a;
+}
+
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,10 +5,18 @@ import {
     Form,
     FormItem,
     Input,
-    Message
+    Message,
+    Header,
+    Container,
+    Aside,
+    Main,
 } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
